4d420915424d98f708baf8cb02fa8ea19a8c2fd4
Temirlan Baibolov <temirlan.baibolov@leaguesofcode.com>
2023-02-09 01:32:53 +0600

    Initial commit

---
+++ src/main/java/com/exercises/m6gradle/M6GradleApplication.java
@@ -0,0 +1,13 @@
+package com.exercises.m6gradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M6GradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(M6GradleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/exercises/m6gradle/M6GradleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exercises.m6gradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class M6GradleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
